a7266525b0662b1fae9c36fb2f5cfba458d46fec
Robert Pedersen <rob84044@gmail.com>
2019-09-23 17:29:22 -0600

    Fleshed out homepaged into smaller components and finished styling our menu item components

--- crwn-clothing/src/App.js
+++ crwn-clothing/src/App.js
@@ -1,5 +1,11 @@
 import React from "react";
 
+// import { Route } from "react-router-dom";
+// <Route exact="Boolean-TrueByDefault" path='/RouteInUrl' component={WhatComponentWeWantToRender} />
+
+// import { Switch } from "react-router-dom";
+// //This allows for control over Routes
+
 import "./App.css";
 
 import HomePage from "./pages/homepage/homepage.component.jsx";
