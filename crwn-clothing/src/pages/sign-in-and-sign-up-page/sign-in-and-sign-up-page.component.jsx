5d4b9b2c3255d8536bc85882782f80aa85dbe02a
RobertPedersenDev <40696979+robpedersendev@users.noreply.github.com>
2019-10-05 17:52:38 -0600

    Added folders for sign in page and componenents. Added styling for form input. My labels should now have a transition affect, but they do not right now. I have applied the styling that should cause them to do the transition, but they are not doing the effect.

---
+++ crwn-clothing/src/pages/sign-in-and-sign-up-page/sign-in-and-sign-up-page.component.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./sign-in-and-sign-up-page.styles.sass";
+
+import SignIn from "../../components/sgin-in/sign-in.component";
+const SignInAndSignUpPage = () => (
+  <div className="sign-in-and-sign-up-page">
+    SIGN IN
+    <SignIn />
+  </div>
+);
+
+export default SignInAndSignUpPage;
