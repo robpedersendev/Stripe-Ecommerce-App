e5166ce2b257897a7e44183d8f6cdfe8736460fe
RobertPedersenDev <40696979+robpedersendev@users.noreply.github.com>
2019-10-06 23:18:17 -0600

    Corrected the onSubmit funciton call and added signup component to the sign in and sign up page

--- crwn-clothing/src/pages/sign-in-and-sign-up-page/sign-in-and-sign-up-page.component.jsx
+++ crwn-clothing/src/pages/sign-in-and-sign-up-page/sign-in-and-sign-up-page.component.jsx
@@ -2,10 +2,12 @@ import React from "react";
 import "./sign-in-and-sign-up-page.styles.sass";
 
 import SignIn from "../../components/sign-in/sign-in.component";
+import SignUp from "../../components/sign-up/sign-up.component";
 const SignInAndSignUpPage = () => (
   <div className="sign-in-and-sign-up-page">
     SIGN IN
     <SignIn />
+    <SignUp />
   </div>
 );
 
