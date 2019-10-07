e80f84680d749e52f318ed44baffa4ed57a8036b
RobertPedersenDev <40696979+robpedersendev@users.noreply.github.com>
2019-10-06 21:00:09 -0600

    Added sign/sign out functionality based on whether or not user is signed in to our firebase database

--- crwn-clothing/src/components/header/header.component.jsx
+++ crwn-clothing/src/components/header/header.component.jsx
@@ -5,7 +5,9 @@ import { ReactComponent as Logo } from "../../assets/crown.svg";
 
 import "./header.styles.sass";
 
-const Header = () => (
+import { auth } from "../../firebase/firebase.utils";
+
+const Header = ({ currentUser }) => (
   <div className="header">
     <Link to="/" className="logo-container">
       <Logo className="logo" />
@@ -17,6 +19,15 @@ const Header = () => (
       <Link className="option" to="/shop">
         Contact
       </Link>
+      {currentUser ? (
+        <div className="option" onClick={() => auth.signOut()}>
+          Sign Out
+        </div>
+      ) : (
+        <Link className="option" to="/signin">
+          Sign In
+        </Link>
+      )}
     </div>
   </div>
 );
