fa674fb88a3557f94c32a34459b675d7c92c214e
RobertPedersenDev <40696979+robpedersendev@users.noreply.github.com>
2019-11-13 23:04:40 -0700

    Added dependencies, removed props from app.js file for header compponent, added redux code to ehader.component.js file to pull userstatus from the redux state, which is currently only null as the action has not been created yet to get the data from firebase for the user data

--- crwn-clothing/src/index.js
+++ crwn-clothing/src/index.js
@@ -1,20 +1,18 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { BrowserRouter } from "react-router-dom";
+import { Provider } from "react-redux";
 
-import { BrowserRouter as Router } from "react-router-dom";
+import store from "./redux/store";
 
 import "./index.css";
 import App from "./App";
 
-import store from "./redux/store";
-
-import { Provider } from "react-redux";
-
 ReactDOM.render(
   <Provider store={store}>
-    <Router>
+    <BrowserRouter>
       <App />
-    </Router>
+    </BrowserRouter>
   </Provider>,
   document.getElementById("root")
 );
