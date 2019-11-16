f27875509703db7e27d434b04d53cd2ba2ceb285
RobertPedersenDev <40696979+robpedersendev@users.noreply.github.com>
2019-11-15 18:05:51 -0700

    Set up the code to be able to handle the add to cart appearance and soon to be functional functions and the redux functionality, once the actions, reducers, and the types are setup for this. I also added some comments to the code to make it easier to follow as well.

--- crwn-clothing/src/components/collection-preview/collection-preview.component.jsx
+++ crwn-clothing/src/components/collection-preview/collection-preview.component.jsx
@@ -10,8 +10,11 @@ const CollectiionPreview = ({ title, items }) => (
     <div className="preview">
       {items
         .filter((item, idx) => idx < 4)
-        .map(({ id, ...otherItemProps }) => (
-          <CollectionItem key={id} {...otherItemProps} />
+        .map(item => (
+          /* 
+            Here we are going to just pass the item to the CollectionItem component as we now have a Redux operation happening within the CollectionItem and need to not deconstruct the item props within the props section of the function component section, but instead within the actual block of the CollectionItem component. You can see this being done by opening up the CollectionItem
+          */
+          <CollectionItem key={item.id} item={item} />
         ))}
     </div>
   </div>
